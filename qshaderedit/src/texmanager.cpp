83ad44af85b888076f19d973e987dc0b15675594
castano <castano@3b072ee4-d119-0410-b31e-677264297654>
2006-08-11 01:20:08 +0000

    Convert texture to BGRA and mirror.

--- qshaderedit/src/texmanager.cpp
+++ qshaderedit/src/texmanager.cpp
@@ -37,7 +37,37 @@ namespace {
 			}
 		}
 	}
-	
+
+	// Taken from Qt.
+	QImage convertToBGRA(const QImage &image)
+	{
+		QImage img = image;
+		if (image.format() != QImage::Format_ARGB32) {
+			img = image.convertToFormat(QImage::Format_ARGB32);
+		}
+
+		if (QSysInfo::ByteOrder == QSysInfo::BigEndian) {
+			// mirror + swizzle
+			QImage res = img.copy();
+			for (int i=0; i < img.height(); i++) {
+				uint *p = (uint*) img.scanLine(i);
+				uint *q = (uint*) res.scanLine(img.height() - i - 1);
+				uint *end = p + img.width();
+				while (p < end) {
+					*q = ((*p << 24) & 0xff000000)
+						 | ((*p >> 24) & 0x000000ff)
+						 | ((*p << 8) & 0x00ff0000)
+						 | ((*p >> 8) & 0x0000ff00);
+					p++;
+					q++;
+				}
+			}
+			return res;
+		}
+		else {
+			return img.mirrored();
+		}
+	}
 	
 	// Image plugin that supports all the image types that Qt supports.
 	class QtImagePlugin
@@ -59,20 +89,21 @@ namespace {
 			if( name.isEmpty() || !image.load(name) ) {
 				image.load(":/images/default.png");
 			}
-
+			
+			image = convertToBGRA(image);
+			
 			*target = GL_TEXTURE_2D;
 			glBindTexture(GL_TEXTURE_2D, obj);
-		
+			
 			if(GLEW_SGIS_generate_mipmap || GLEW_VERSION_1_4) {
 				glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
-		//		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.bits());
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, image.bits());
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.bits());
 				
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 			}
 			else {
 			//	gluBuild2DMipmaps(GL_TEXTURE_2D, 4, image.width(), image.height(), GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, image.bits());				
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, image.bits());
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, GL_BGRA, GL_UNSIGNED_BYTE, image.bits());
 				
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 			}
