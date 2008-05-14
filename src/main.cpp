9fd1182abe2f4b0721c634744eac1dfae4aaecfe
castano <castano@3b072ee4-d119-0410-b31e-677264297654>
2008-05-14 06:13:15 +0000

    Add license headers.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
+/*
+    QShaderEdit - Simple multiplatform shader editor
+    Copyright (C) 2007 Ignacio Castaño <castano@gmail.com>
+    Copyright (C) 2007 Lars Uebernickel <larsuebernickel@gmx.de>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 #include "qshaderedit.h"
 
@@ -13,7 +32,7 @@ int main(int argc, char **argv)
 {
 #if defined(Q_WS_X11)
 	// This causes lockups in some X servers.
-	XInitThreads();
+	//XInitThreads();
 #endif
 	
     QApplication app(argc, argv);
